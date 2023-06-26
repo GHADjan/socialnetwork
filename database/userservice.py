4a49ac9f6a1a0501e46270b3de0bcd37fe2a1386
GHADjan <jandosbrawl@gmail.com>
2023-06-26 21:32:51 +0500

    Соц сеть готова

--- database/userservice.py
+++ database/userservice.py
@@ -10,26 +10,37 @@ def register_user_db(**user_data):
 
 
 # Проверка пользователя по email
-def check_user_db():
-    pass
+def check_user_db(email):
+    user = User.query.filter_by(email=email).first()
+    return user is not None
 
 
 # Проверка пароля пользовтеля
 def check_user_password_db(email, password):
-    pass
+    user = User.query.filber_by(email=email).first()
+    if user is not None:
+        user_password = Passwords.query.filter_by(user_id=user.user_id).first()
+        if user_password is not None:
+            return user_password.password == password
+    return False
 
 
 # Получение всех пользователей из базы
 def get_all_users_db():
-    pass
+    user = User.query.all()
+    return user
 
 
 # Получить конкретного пользователя
 def get_exact_user_db(user_id):
-    pass
+    user = User.query.filter_by(user_id=user_id).first()
+    return user
 
 
 # Удалить пользовтеля из базы
 def delete_user_db(user_id):
-    pass
+    user = User.query.filter_by(user_id=user_id).first()
+    if user is not None:
+        db.session.delete(user)
+        db.session.commit()
 
